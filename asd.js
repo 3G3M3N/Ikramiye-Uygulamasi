72b98acd23ea2ec7d68c0e6f2403ea0b1af8514f
363/-/3/ <83331577+3G3M3N@users.noreply.github.com>
2022-09-22 22:19:55 +0300

    Update asd.js

--- asd.js
+++ asd.js
@@ -1,1 +1,21 @@
+let sayı = (Math.random() * 90) + 10 
+sayı = Math.floor(sayı)
+console.log(sayı)
 
+let tahmin = parseInt(prompt("2 basamaklı sayınızı giriniz: "))
+
+let sayı_onlar = sayı / 10 
+let sayı_birler = sayı % 10 
+
+let tahminonlar = tahmin / 10 
+let tahmin_birler = tahmin % 10 
+
+if (tahmin == sayı){
+    console.log("Tebrikler 10.000 TL kazandınız...")
+}else if (tahminonlar == sayı_onlar || tahmin_birler == sayı_birler){
+    console.log("Tebrikler 5000 TL kazandınız...")
+}else if(tahminonlar == sayı_birler || tahmin_birler == sayı_onlar){
+    console.log("Tebrikler 1000 TL kazandınız...")
+}else{
+    console.log("Maalesef bir şey kazanamadınız, cevap " +  `${sayı}`  + " idi...")
+}
