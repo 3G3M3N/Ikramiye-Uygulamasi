8a92ef7813883c8063dd4cd7fc1d502493197535
3G3M3N <83331577+3G3M3N@users.noreply.github.com>
2023-06-03 21:19:46 +0300

    Update yukariasagisayitahmin.js

--- yukariasagisayitahmin.js
+++ yukariasagisayitahmin.js
@@ -1,1 +1,17 @@
-
+let sayı = Math.floor((Math.random()*100))
+console.log(sayı)
+let tahmin = parseInt(prompt("0-100'e Sayı Tahmin Ediniz"))
+let hak = 4
+while (hak>=0) {
+    if (tahmin==sayı) {
+        console.log("Tebrikler Doğru Yanıt.")
+    } else if (tahmin>sayı) {
+        tahmin = parseInt(prompt("İn innn"))
+    }else if (tahmin<sayı) {
+        tahmin = parseInt(prompt("Çık çıkkkk"))
+    }
+    hak -= 1
+}
+if (hak=0) {
+    console.log("Maalesef Kaybettiniz, Sayı",sayı,"idi")
+}
